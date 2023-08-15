3a775040a9ee494609422d64c97f15a2254a3aaa
Balaguru Sivasambagupta <bala1802@live.com>
2023-08-15 10:38:47 +0530

    removed albumentation

--- dataset.py
+++ dataset.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn.functional as F
 import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 import pytorch_lightning as pl
 
 import config
-from dataTransformer import DATA_AUGMENTATION, construct_transform_object
+# from dataTransformer import DATA_AUGMENTATION, construct_transform_object
 
 class CIFAR10DataModule(pl.LightningDataModule):
     
@@ -22,10 +23,17 @@ class CIFAR10DataModule(pl.LightningDataModule):
         datasets.CIFAR10(self.data_dir, train=False, download=True)
     
     def setup(self, stage=None):
-        self.train_dataset = DATA_AUGMENTATION(data_dir=self.data_dir, train=True, 
-                          transform=construct_transform_object(train=True))
-        self.test_dataset = DATA_AUGMENTATION(data_dir=self.data_dir, train=False, 
-                          transform=construct_transform_object(train=False))
+        # self.train_dataset = DATA_AUGMENTATION(data_dir=self.data_dir, train=True, 
+        #                   transform=construct_transform_object(train=True))
+        # self.test_dataset = DATA_AUGMENTATION(data_dir=self.data_dir, train=False, 
+        #                   transform=construct_transform_object(train=False))
+        self.train_dataset = datasets.CIFAR10(self.data_dir, train=True, download=False, 
+                                              transform=transforms.Compose([
+                                              transforms.RandomVerticalFlip(),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor()]))
+        self.train_dataset = datasets.CIFAR10(self.data_dir, train=False, download=False,
+                                              transform=transforms.ToTensor())
         
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
