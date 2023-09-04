46d2672582f66f66d24b0d434b20edbcd8177ccc
bala1802 <bala1802@live.com>
2023-09-04 19:10:31 +0530

    Added comments to the cifar_datamodule.py

--- tools/data/cifar_datamodule.py
+++ tools/data/cifar_datamodule.py
@@ -1,3 +1,14 @@
+'''
+Author Name: Balaguru Sivasambagupta
+Autor Email ID: bala1802@live.com
+Github: https://github.com/bala1802
+'''
+
+'''
+Purpose of this Script:
+    - A LightningDataModule is initialized to work on the CIFAR10 dataset
+'''
+
 from typing import Optional, Union
 
 import albumentations as A
@@ -9,14 +20,8 @@ from .components.cifar10 import CIFAR10, make_transform
 
 
 class CIFAR10DataModule(LightningDataModule):
-    def __init__(
-        self,
-        data_dir: str = "./data",
-        batch_size: int = 512,
-        num_workers: int = 0,
-        pin_memory: bool = False,
-        train_augments: Union[A.Compose, None] = None,
-    ):
+    def __init__(self, data_dir: str = "./data", batch_size: int = 512, num_workers: int = 0, 
+                 pin_memory: bool = False, train_augments: Union[A.Compose, None] = None):
         super().__init__()
 
         # this line allows to access init params with 'self.hparams' attribute
@@ -35,17 +40,25 @@ class CIFAR10DataModule(LightningDataModule):
     def num_classes(self):
         return 10
 
+    '''
+        - The dataset is downloaded and stored inside the specified directory
+        - Both the `train` and `test/val` datasets are downloaded
+    '''
     def prepare_data(self):
-        # download
         datasets.CIFAR10(self.hparams.data_dir, train=True, download=True)
         datasets.CIFAR10(self.hparams.data_dir, train=False, download=True)
 
+    '''
+     A simple validation is done to check whether the `train` and `test` data are already instantiated or not
+    '''
     def setup(self, stage=None) -> None:
-        # load  only if not loaded already
         if not self.data_train and not self.data_val:
             self.data_train = datasets.CIFAR10(self.hparams.data_dir, train=True)
             self.data_val = datasets.CIFAR10(self.hparams.data_dir, train=False)
 
+    '''
+    `train` data loader instantiation
+    '''
     def train_dataloader(self):
         return DataLoader(
             dataset=CIFAR10(self.data_train, self.train_transforms),
@@ -55,6 +68,9 @@ class CIFAR10DataModule(LightningDataModule):
             shuffle=True,
         )
 
+    '''
+    `validation` data loader instantiation
+    '''
     def val_dataloader(self):
         return DataLoader(
             dataset=CIFAR10(self.data_val, self.val_transforms),
